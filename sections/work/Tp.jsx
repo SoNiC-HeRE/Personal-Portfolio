d4eafaa423c7117a253587a19460dc481c7068d4
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2023-04-22 20:34:02 +0530

    Minor Bugs

--- sections/work/Tp.jsx
+++ sections/work/Tp.jsx
@@ -11,7 +11,7 @@ const Tp = () => {
     className='w-full'
 >
  <h3 className='flex gap-1 font-medium text-xl font-titleFont text-white'>
-    Student Coordinator <span className='text-[#66FCF1] tracking-wide'>@GitHub</span></h3>
+    Student Coordinator <span className='text-[#66FCF1] tracking-wide'>@T&P</span></h3>
     <p className='text-sm mt-1 font-medium text-secondary-white'>1st November 2022 to Present</p>
     <ul className='mt-6 flex flex-col gap-3'>
         <li className='text-base flex gap-2 text-white'><span className='text-[#66fcf1]'><TiArrowForward /></span>
